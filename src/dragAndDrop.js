7d7c93c7202a80ffb28ae80c433b17e159194883
Guillaume Wells <wellsguillaume@gmail.com>
2022-11-05 11:31:07 +0100

    feat(main): Kiss animation drag

--- src/dragAndDrop.js
+++ src/dragAndDrop.js
@@ -33,16 +33,21 @@ export default class DragAndDrop
 
     mouseUp(e) {
         e.preventDefault();
-        if (e.target.id == 'dragged-el' === false)
-            return;
         this.isDragging = false;
+        if (e.target.id == 'dragged-el' === false) {
+            this.maxEl == null;
+            this.draggedEl.remove();
+            return;
+        }
         let rect = this.draggedEl.getBoundingClientRect();
         this.draggedEl.remove();
         this.drop(rect);
     }
 
+
     mouseMove(e) {
         e.preventDefault();
+        e.stopPropagation();
         if (this.isDragging == true) {
             let cursorPositionX = e.clientX;
             let cursorPositionY = e.clientY;
@@ -51,15 +56,52 @@ export default class DragAndDrop
             this.draggedEl.style.top = `${parseInt(this.draggedEl.style.top.replace('px', ''), 10) + (cursorPositionY - this.initialY)}px`;
             this.initialX = cursorPositionX;
             this.initialY = cursorPositionY;
-            let rect = this.draggedEl.getBoundingClientRect();
-            if (rect.bottom > window.innerHeight && (window.scrollY + window.innerHeight) < document.body.scrollHeight) {
-                window.scroll(0, window.scrollY + 5);
-                this.draggedEl.style.top = `${parseInt(this.draggedEl.style.top.replace('px', ''), 10) + (5)}px`;
-            }
-            if (rect.top < 0 && window.scrollY > 0) {
-                window.scroll(0, window.scrollY - 5);
-                this.draggedEl.style.top = `${parseInt(this.draggedEl.style.top.replace('px', ''), 10) - (5)}px`;
-            }
+            //while  (rect.top < 0 && window.scrollY > 0)
+			let start = Date.now(); // remember start time
+
+            let timer = setInterval(function() {
+                let timePassed = Date.now() - start;
+                let rect = this.draggedEl.getBoundingClientRect();
+
+                if ((rect.top >= 0 && rect.bottom < window.innerHeight)) {
+                    console.log('biib');
+                    clearInterval(timer);
+                    return;
+                } else if (rect.top < 0 && scrollY > 0) {
+                    console.log('top');
+                    window.scroll(0, window.scrollY - 1);
+                    this.draggedEl.style.top = `${parseInt(this.draggedEl.style.top.replace('px', ''), 10) - (1)}px`;
+                } else if (rect.bottom <= document.body.scrollHeight && rect.bottom > window.innerHeight) {
+                    console.log(document.body.scrollHeight);
+                    console.log(rect.bottom);
+                    console.log('down');
+                    window.scroll(0, window.scrollY + 1);
+                    this.draggedEl.style.top = `${parseInt(this.draggedEl.style.top.replace('px', ''), 10) + (1)}px`;
+                } else {
+                    console.log('out');
+                    clearInterval(timer);
+                }
+                // } else if (window.scrollY > 0 && rect.top < 0) {
+                //     console.log('up');
+                //     window.scroll(0, window.scrollY - 1);
+                //     this.draggedEl.style.top = `${parseInt(this.draggedEl.style.top.replace('px', ''), 10) - (1)}px`;
+                // } else if (window.scrollY < window.innerHeight && rect.bottom > window.innerHeight){
+                //     console.log('down');
+                //     window.scroll(0, window.scrollY + 1);
+                //     this.draggedEl.style.top = `${parseInt(this.draggedEl.style.top.replace('px', ''), 10) + (1)}px`;
+                // } else {
+                //     clearInterval(timer);
+                //     console.log('baby');
+                // }
+            }.bind(this), 20);
+            // if (rect.bottom > window.innerHeight && (window.scrollY + window.innerHeight) < document.body.scrollheight) {
+            //     window.scroll(0, window.scrollY + 5);
+            //     this.draggedEl.style.top = `${parseInt(this.draggedEl.style.top.replace('px', ''), 10) + (5)}px`;
+            // }
+            // if (rect.top < 0 && window.scrollY > 0) {
+            //     window.scroll(0, window.scrollY - 5);
+            //     this.draggedEl.style.top = `${parseInt(this.draggedEl.style.top.replace('px', ''), 10) - (5)}px`;
+            // }
         }
     }
 
